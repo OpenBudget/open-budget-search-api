9e5e7015d24c43a055b7a60a21a226d2cf8efc1d
Jonathan Roizin <Jonathan Roizin>
2016-11-14 13:23:50 +0200

    converted to flask

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
 import elastic
 from elastic import logger
-import tornado.ioloop
-import tornado.web
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+@app.route('/', methods = ['GET'])
+def main_handler():
+    if request.method == 'GET':
+        return "Hello, world"
 
-class IndexDocumentHandler(tornado.web.RequestHandler):
-    def post(self, type_name):
-        data = tornado.escape.json_decode(self.request.body)
-        elastic.index_doc(type_name, data)
+@app.route('/index/<string:type_name>', methods = ['POST'])
+def index_document_handler(type_name):
+    data = request.get_json()
+    elastic.index_doc(type_name, data)
 
-
-class SearchHandler(tornado.web.RequestHandler):
-    def get(self, types, search_term, from_date, to_date, size, offset):
-        types_formatted = str(types).split(",")
-        result = {}
-        try:
-            result = elastic.search(types_formatted, search_term, from_date, to_date, size, offset)
-        except Exception as e:
-            self.write(str(e))
-            logger.exception("Error searching %s for tables: %s " % (search_term, str(types)))
-        self.write(result)
-
-
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-        (r"/index/(?P<type_name>[^\/]+)", IndexDocumentHandler),
-        (r"/search/(?P<types>[^\/]+)/?(?P<search_term>[^\/]+)?/?(?P<from_date>[^\/]+)?/(?P<to_date>[^\/]+)/?(?P<size>[^\/]+)?/?(?P<offset>[^\/]+)?", SearchHandler)
-
-
-    ])
+@app.route('/search/<string:types>/<string:search_term>/<string:from_date>/<string:to_date>/<string:size>/<string:offset>', methods=['GET'])
+def search_handler(types, search_term, from_date, to_date, size, offset):
+    types_formatted = str(types).split(",")
+    try:
+        result = elastic.search(types_formatted, search_term, from_date, to_date, size, offset)
+    except Exception as e:
+        logger.exception("Error searching %s for tables: %s " % (search_term, str(types)))
+        return str(e)
+    return jsonify(result)
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    app.run()
