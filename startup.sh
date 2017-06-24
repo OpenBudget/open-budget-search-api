d7689696e783f4f596f063132a30cc76cc01e753
Adam Kariv <adam.kariv@gmail.com>
2017-06-24 16:28:49 +0300

    Reindex

--- startup.sh
+++ startup.sh
@@ -3,7 +3,8 @@ set -e
 
 cd $APP_PATH
 sleep 10
-# ADD if necessary: python3 load_data.py clean
+# ADD if necessary: 
+python3 load_data.py clean
 (while true ; do python3 load_data.py all ; sleep 3600 ; done )&
 
 $APP_PATH/entrypoint.sh
