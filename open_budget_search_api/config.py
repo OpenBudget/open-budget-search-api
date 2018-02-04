5228f8c0a930ea007fa81205056791ef2c9315ee
Adam Kariv <adam.kariv@gmail.com>
2018-02-04 08:15:24 +0200

    Re-add entities

--- open_budget_search_api/config.py
+++ open_budget_search_api/config.py
@@ -8,14 +8,14 @@ es_connection_string = '{}:{}'\
     .format(ES_HOST, ES_PORT)
 
 INDEX_NAME = os.environ.get('INDEX_NAME', 'budgetkey')
-ES_SERVERS_LIST = [{"host": ES_HOST, "port": int(ES_PORT)}]
+ES_SERVERS_LIST = [ES_HOST]
 DEFAULT_TIMEOUT = 60
 
 SEARCHABLE_DATAPACKAGES = [
     "http://next.obudget.org/datapackages/people/aggregated/datapackage.json",
     "http://next.obudget.org/datapackages/budget/national/changes/full/datapackage.json",
     "http://next.obudget.org/datapackages/procurement/tenders/processed/datapackage.json",
-  #  "http://next.obudget.org/datapackages/entities/scored/datapackage.json",
+    "http://next.obudget.org/datapackages/entities/scored/datapackage.json",
 
     "http://next.obudget.org/datapackages/budget/national/processed/connected-items/datapackage.json",
     "http://next.obudget.org/datapackages/procurement/spending/latest-contract-spending/datapackage.json",
