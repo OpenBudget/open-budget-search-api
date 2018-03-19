28d22a4a6f2cf6df2155a0c0c80b980337421aab
Adam Kariv <adam.kariv@gmail.com>
2018-03-19 22:02:13 +0200

    Fix locations of source datapackages

--- open_budget_search_api/config.py
+++ open_budget_search_api/config.py
@@ -7,15 +7,20 @@ INDEX_NAME = os.environ.get('INDEX_NAME', 'budgetkey')
 ES_SERVERS_LIST = [{"host": ES_HOST, "port": int(ES_PORT)}]
 DEFAULT_TIMEOUT = 60
 
-SEARCHABLE_DATAPACKAGES = [
-    "http://next.obudget.org/datapackages/people/aggregated/datapackage.json",
-    "http://next.obudget.org/datapackages/budget/national/changes/full/datapackage.json",
-    "http://next.obudget.org/datapackages/procurement/tenders/processed/datapackage.json",
-    "http://next.obudget.org/datapackages/entities/scored/datapackage.json",
+DATAPACKAGE_BASE = 'http://next.obudget.org/datapackages/budgetkey/{}/datapackage.json'
 
-    "http://next.obudget.org/datapackages/budget/national/processed/connected-items/datapackage.json",
-    "http://next.obudget.org/datapackages/procurement/spending/latest-contract-spending/datapackage.json",
-    "http://next.obudget.org/datapackages/supports/by-request-year/datapackage.json",
+SEARCHABLE_DATAPACKAGES = [
+    DATAPACKAGE_BASE.format(doc_type)
+    for doc_type in [
+        'people',
+        'tenders',
+        'entities',
+        'contract-spending',
+        'national-budget-changes',
+        'supports',
+        'reports',
+        'budget'
+    ]
 ]
 NON_SEARCHABLE_DATAPACKAGES = [
     'http://next.obudget.org/datapackages/budgetkey/documents/datapackage.json',
