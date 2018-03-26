8f878a820f3406eceec9fb0a17f70986d46bd880
Ori Hoch <ori@uumpa.com>
2018-03-26 13:06:25 +0300

    modify search query to support total aggregations and search by time range fields

--- open_budget_search_api/elastic.py
+++ open_budget_search_api/elastic.py
@@ -1,21 +1,28 @@
 import elasticsearch
+from copy import deepcopy
 
 from .data_sources import sources
 from .config import INDEX_NAME, get_es_client
 
 
-def prepare_typed_query(type_names, term, from_date, to_date, search_size, offset):
+def prepare_base_query(type_names, term):
     search_fields = [sources[type_name].search_fields for type_name in type_names]
     search_fields = list(set().union(*search_fields))
     body = {
         'query': {
             'function_score': {
                 'query': {
-                    'multi_match': {
-                        'query': term,
-                        'fields': search_fields,
-                        'type': 'best_fields',
-                        'operator': 'and'
+                    'bool': {
+                        'must': [
+                            {
+                                'multi_match': {
+                                    'query': term,
+                                    'fields': search_fields,
+                                    'type': 'best_fields',
+                                    'operator': 'and'
+                                }
+                            }
+                        ]
                     }
                 },
                 'boost_mode': 'multiply',
@@ -30,7 +37,31 @@ def prepare_typed_query(type_names, term, from_date, to_date, search_size, offse
             'type_totals': {
                 'terms': {'field': '_type'}
             }
-        },
+        }
+    }
+    for ds in sources.values():
+        if ds.date_fields.get('from') and ds.date_fields.get('to'):
+            body['aggs']['months_{}'.format(ds.type_name)] = {'date_histogram': {'field': ds.date_fields['from'],
+                                                                                 'interval': 'month',
+                                                                                 'min_doc_count': 1,
+                                                                                 'format': 'yyyy-MM'}}
+        elif ds.date_fields.get('year'):
+            body['aggs']['years_{}'.format(ds.type_name)] = {'histogram': {'field': ds.date_fields['year'], 'interval': 1}}
+    return body
+
+
+def prepare_totals_query(body):
+    body = deepcopy(body)
+    body.update({
+        'size': 0
+    })
+    import json; print(json.dumps(body, indent=2))
+    return body
+
+
+def prepare_search_query(body, from_date, to_date, search_size, offset):
+    body = deepcopy(body)
+    body.update({
         'size': int(search_size),
         'from': int(offset),
         'highlight': {
@@ -38,33 +69,36 @@ def prepare_typed_query(type_names, term, from_date, to_date, search_size, offse
                 '*': {}
             }
         }
+    })
+    date_fields_query = {
+        'bool': {
+            'should': []
+        }
     }
-
+    # TODO: extract year as integer from from_data and to_date
+    from_year, to_year = 2005, 2007
+    body['query']['function_score']['query']['bool']['must'].append(date_fields_query)
     for ds in sources.values():
-        for range_name, field_name in ds.date_fields.items():
-            if range_name == 'year':
-                body['aggs']['years_{}'.format(ds.type_name)] = {'histogram': {'field': field_name, 'interval': 1}}
-            elif range_name == 'from':
-                body['aggs']['months_{}'.format(ds.type_name)] = {'date_histogram': {'field': field_name,
-                                                                                     'interval': 'month',
-                                                                                     'min_doc_count': 1,
-                                                                                     'format': 'yyyy-MM'}}
-
-    # if False:#ds.is_temporal:
-    #     body['aggs']['stats_per_month'] = {
-    #         'date_histogram': {
-    #             'field': ds.date_fields['from'],
-    #             'interval': 'month',
-    #             'min_doc_count': 1
-    #         }
-    #     }
-    #     range_obj = ds.range_structure
-    #     range_obj[ds.date_fields['from']]['gte'] = from_date
-    #     range_obj[ds.date_fields['to']]['lte'] = to_date
-    #     bool_filter = body['aggs']['filtered']['filter']['bool']
-    #     bool_filter['must'] = [{
-    #         'range': range_obj
-    #     }]
+        if ds.date_fields.get('from') and ds.date_fields.get('to'):
+            date_fields_query['bool']['should'].append({
+                'bool': {
+                    'must': [
+                        {'type': {'value': ds.type_name}},
+                        {'range': {ds.date_fields['from']: {'gte': from_date},
+                                   ds.date_fields['to']: {'lte': to_date}}}
+                    ]
+                }
+            })
+        elif ds.date_fields.get('year'):
+            date_fields_query['bool']['should'].append({
+                'bool': {
+                    'must': [
+                        {'type': {'value': ds.type_name}},
+                        {'range': {ds.date_fields['year']: {'gte': from_year, 'lte': to_year}}}
+                    ]
+                }
+            })
+    import json; print(json.dumps(body, indent=2))
     return body
 
 
@@ -135,9 +169,13 @@ def search(types, term, from_date, to_date, size, offset):
     for type_name in types:
         if type_name not in sources:
             return {'message': 'not a real type %s' % type_name}
-    query = prepare_typed_query(types, term, from_date, to_date, size, offset)
-    results = get_es_client().search(index=INDEX_NAME, doc_type=','.join(types), body=query)
-    overalls = results['aggregations']['type_totals']['buckets']
+
+    base_query = prepare_base_query(types, term)
+
+    search_query = prepare_search_query(base_query, from_date, to_date, size, offset)
+    search_results = get_es_client().search(index=INDEX_NAME, doc_type=','.join(types), body=search_query)
+
+    overalls = search_results['aggregations']['type_totals']['buckets']
     overalls = dict(
         (i['key'], i['doc_count'])
         for i in overalls
@@ -147,7 +185,8 @@ def search(types, term, from_date, to_date, size, offset):
         ret_val['search_counts'][type_name.replace('-', '')] = {
             'total_overall': overalls.get(type_name, 0),
         }
-    for hit in results['hits']['hits']:
+
+    for hit in search_results['hits']['hits']:
         ret_val['search_results'].append({
             'source': merge_highlight_into_source(hit['_source'], hit['highlight']),
             'highlight': {},
@@ -155,7 +194,10 @@ def search(types, term, from_date, to_date, size, offset):
             'score': hit['_score'],
         })
 
-    for agg_name, agg_data in results['aggregations'].items():
+    totals_query = prepare_totals_query(base_query)
+    total_results = get_es_client().search(index=INDEX_NAME, doc_type=','.join(types), body=totals_query)
+
+    for agg_name, agg_data in total_results['aggregations'].items():
         range_name = None
         if agg_name.startswith('years_'):
             range_name = 'year'
