44b0115906e83cee902fe502b159b10d284c7d85
Adam Kariv <adam.kariv@gmail.com>
2019-12-26 14:44:15 +0200

    Bump apies

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     packages=PACKAGES,
     include_package_data=True,
     install_requires=[
-        'apies>=1.1.6'
+        'apies>=1.1.7'
     ],
     tests_require=TESTS_REQUIRE,
     extras_require={'develop': TESTS_REQUIRE},
